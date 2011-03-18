062111f8469ed39d9c3a5b58c0793cef70f70305
Jurriën Stutterheim <j.stutterheim@me.com>
2011-03-18 16:45:14 +0100

    Full Maybe implementation!

--- Data/Maybe.php
+++ Data/Maybe.php
@@ -7,10 +7,8 @@ abstract class Maybe implements IMonad, IFunctor, IShow {
     }
 
     public static function bind(IMonad $ma, Closure $a2mb) {
-        self::isMaybe($ma);
-
         // If we have a Nothing, we return Nothing
-        if ($ma instanceof Nothing) return new Nothing();
+        if (self::isNothing($ma)) return new Nothing();
 
         // Otherwise we have a Just and we call the
         // a2mb function
@@ -31,13 +29,68 @@ abstract class Maybe implements IMonad, IFunctor, IShow {
     }
 
     public static function fmap(Closure $a2b, $fa) {
-        self::isMaybe($fa);
-
         // If we have a Nothing, we return Nothing
-        if ($fa instanceof Nothing) return new Nothing();
+        if (self::isNothing($fa)) return new Nothing();
 
         // TODO: Add check for $a2b
         return new Just($a2b($fa->a()));
+    }
+    
+    public static function maybe_($b, Closure $f, Maybe $a) {
+        if (self::isNothing($a)) return $b;
+
+        return $f($a->a());
+    }
+
+    public static function fromJust(Maybe $a) {
+        if (self::isNothing($a)) {
+            throw new MaybeException("Nothing!");
+        }
+
+        return $a->a();
+    }
+
+    public static function fromMaybe($d, Maybe $a) {
+        if (self::isNothing($a)) return $d;
+        return $a->a();
+    }
+
+    public static function maybeToList(Maybe $a) {
+        if (self::isNothing($a)) return new Nil();
+        return new Cons($a->a(), new Nil());
+    }
+
+    public static function listToMabye(HList $l) {
+        if ($l instanceof Nil) return new Nothing();
+        return new Just(HList::head($l));
+    }
+
+    public static function catMaybes(HList $l) {
+        return HList::foldr(function ($x, $xs) {
+            if (self::isJust($x)) { $xs[] = $x->a(); }
+            return $xs;
+        }, new Nil(), $l);
+    }
+
+    public static function mapMaybe(Closure $f, HList $as) {
+        if ($as instanceof Nil) return new Nil();
+        $rs = self::mapMaybe($f, HList::tail($as));
+        $m = $f(HList::head($as));
+        if (self::isNothing($m)) {
+            return $rs;
+        } else {
+            return new Cons($m->a(), $rs);
+        }
+    }
+
+    public static function isJust(Maybe $a) {
+        self::isMaybe($a);
+        return ($a instanceof Just);
+    }
+
+    public static function isNothing(Maybe $a) {
+        self::isMaybe($a);
+        return ($a instanceof Nothing);
     }
 
     public function __toString() {
@@ -66,3 +119,5 @@ final class Just extends Maybe {
         return "Just " . $show->a();
     }
 }
+
+class MaybeException extends Exception {}
